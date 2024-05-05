b9e852c7f68645a11c57e4a3df26327753d153ec
ortizjulian <julian.ortixs@gmail.com>
2024-05-05 17:38:36 -0500

    home_view

--- app/src/main/java/com/financiat/upb/Datos.java
+++ app/src/main/java/com/financiat/upb/Datos.java
@@ -3,5 +3,22 @@ package com.financiat.upb;
 import android.app.Application;
 
 public class Datos extends Application {
+    String ingresos = "$10.000";
+    String gastos = "$20.000";
 
+    public String getGastos() {
+        return gastos;
+    }
+
+    public void setGastos(String gastos) {
+        this.gastos = gastos;
+    }
+
+    public String getIngresos() {
+        return ingresos;
+    }
+
+    public void setIngresos(String ingresos) {
+        this.ingresos = ingresos;
+    }
 }
